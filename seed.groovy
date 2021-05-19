3ea8cb0bc09df0fcb2e29ab5f4d4f71b8e92d652
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-19 16:22:05 +0530

    new

---
+++ seed.groovy
@@ -0,0 +1,31 @@
+folder('CI-Pipelines') {
+  displayName('CI Pipelines')
+  description('CI Pipelines')
+}
+
+def component = ["frontend","users","login","todo"];
+
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/swathi-zelarsoft680/'+j+'.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+}
