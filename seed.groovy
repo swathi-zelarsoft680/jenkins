83b1f7b975f44b838135989d6af7ed0a00e500ed
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-19 16:42:35 +0530

    m

--- seed.groovy
+++ seed.groovy
@@ -14,7 +14,7 @@ for (i in 0..count) {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/swathi-zelarsoft680/'+j+'.git')
+              'url'('https://github.com/sireeshazs/'+j+'.git')
             }
           }
           'branches' {
